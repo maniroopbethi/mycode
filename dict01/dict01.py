e27ccf1cc82897ea607458c9bcc424b53eae0330
maniroopbethi <maniroop.bethi@gmail.com>
2019-06-24 11:51:43 -0400

    dict

---
+++ dict01/dict01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def main():
+
+    switch = {'hostname': 'sw1', 'ip': '10.0.1.1', 'version': '1.2', 'vendor': 'cisco'}
+
+    print(switch['hostname'])
+    print(switch['ip'])
+    switch["lynx"] = "1.1.1.1"
+    print(switch['lynx'])
+    print(switch.get("Hughes"))
+
+main()
